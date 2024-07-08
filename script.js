a873ad9a6ad5d2965b016e20287ac18cdb537a50
MD Tariqul Islam <ittarek551@gmail.com>
2024-07-08 15:55:58 +0600

    add navbar menu item

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+let menuToggle = document.querySelector(".menuToggle");
+let menuItems = document.querySelector(".menuItems");
+let menuItemsLi = document.querySelectorAll(".menuItems li");
+let menuToggleclose = document.querySelector(".menuToggleclose");
+
+menuToggle.onclick = function () {
+  menuItems.classList.toggle("active");
+};
+menuToggleclose.onclick = function () {
+  menuItems.classList.remove("active");
+};
+
+function activeLink() {
+  menuItemsLi.forEach(item => item.classList.remove("active"));
+  this.classList.add("active");
+}
+
+menuItemsLi.forEach(item => item.addEventListener("click", activeLink));
+
+// slider
 const slides = document.querySelectorAll(".slide");
 const slider = document.getElementById("slider");
 const currentPageElem = document.getElementById("current-page");
@@ -17,15 +37,15 @@ document.getElementById("prev").addEventListener("click", () => {
   goToSlide(currentSlide - 1);
 });
 
-document.getElementById("go-to-page").addEventListener("click", () => {
-  const pageNumber = parseInt(pageInput.value, 10);
-  if (pageNumber >= 1 && pageNumber <= slides.length) {
-    goToSlide(pageNumber - 1); 
-    pageInput.value = ""; 
-  } else {
-    alert(`Please enter a number between 1 and ${slides.length}.`);
-  }
-});
+// document.getElementById("go-to-page").addEventListener("click", () => {
+//   const pageNumber = parseInt(pageInput.value, 10);
+//   if (pageNumber >= 1 && pageNumber <= slides.length) {
+//     goToSlide(pageNumber - 1);
+//     pageInput.value = "";
+//   } else {
+//     alert(`Please enter a number between 1 and ${slides.length}.`);
+//   }
+// });
 
 function goToSlide(n) {
   currentSlide = (n + slides.length) % slides.length;
@@ -41,4 +61,3 @@ function updatePagination() {
 }
 
 updatePagination();
-
